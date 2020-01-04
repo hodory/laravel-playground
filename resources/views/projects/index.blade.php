de04038691d4098f347c7c963bcef34c3400615e
hodory <mo02@outlook.kr>
2020-01-04 17:33:37 +0900

    edit : edit layout and tailwind css

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,13 +6,15 @@
         <a href="/projects/create">New Project</a>
     </div>
 
-<ul>
-    @forelse($projects as $project)
-        <li>
-            <a href="{{$project->path()}}">{{$project->title}}</a>
-        </li>
-    @empty
-        <li>No projects yet.</li>
-    @endforelse
-</ul>
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
+                <h3 class="font-normal text-xl py-6">{{$project->title}}</h3>
+
+                <div class="text-grey">{{ Str::limit($project->description, 100)}}</div>
+            </div>
+        @empty
+            <div>No projects yet.</div>
+        @endforelse
+    </div>
 @endsection
