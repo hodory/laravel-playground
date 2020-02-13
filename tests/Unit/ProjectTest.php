91618eaeb85eab1d22d827cc8e7ae2ad0c25c9e1
hodory <mo02@outlook.kr>
2020-02-13 23:44:08 +0900

    feat : Layered Tests and Invitations

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -43,5 +44,16 @@ class ProjectTest extends TestCase
 
         $this->assertCount(1, $project->tasks);
         $this->assertTrue($project->tasks->contains($task));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_invite_a_user()
+    {
+        $project = factory('App\Project')->create();
+        $project->invite($user = factory(User::class)->create());
+
+        $this->assertTrue($project->members->contains($user));
     }
 }
