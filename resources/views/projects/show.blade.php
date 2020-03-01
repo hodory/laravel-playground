7e861d69295f0329e5eb8ce5cca393c35e03a92c
hodory <mo02@outlook.kr>
2020-03-01 22:12:33 +0900

    refactor : migrate react.js code to vue.js

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,77 +1,91 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
 @section('content')
-    <header class="flex items-center mb-4 py-4">
+    <header class="flex items-center mb-6 pb-4">
         <div class="flex justify-between items-end w-full">
-            <p class="text-default text-sm font-normal">
-                <a href="/projects" class="text-default text-sm font-normal no-underline">My Projects</a>
-                / {{$project->title}}
+            <p class="text-muted font-light">
+                <a href="/projects" class="text-muted no-underline hover:underline">My Projects</a>
+                / {{ $project->title }}
             </p>
+
             <div class="flex items-center">
                 @foreach ($project->members as $member)
                     <img
-                        src="{{gravatar_url($member->email)}}"
-                        alt="{{$member->name}}'s avatar" class="rounded-full w-8 mr-2">
+                        src="{{ gravatar_url($member->email) }}"
+                        alt="{{ $member->name }}'s avatar"
+                        class="rounded-full w-8 mr-2">
                 @endforeach
+
                 <img
-                    src="{{gravatar_url($project->owner->email)}}"
-                    alt="{{$project->owner->name}}'s avatar" class="rounded-full w-8 mr-2">
+                    src="{{ gravatar_url($project->owner->email) }}"
+                    alt="{{ $project->owner->name }}'s avatar"
+                    class="rounded-full w-8 mr-2">
+
+                <a href="{{ $project->path().'/edit' }}" class="button ml-4">Edit Project</a>
             </div>
-            <a href="{{$project->path().'/edit'}}" class="button ml-4">Edit Project</a>
         </div>
     </header>
+
     <main>
         <div class="lg:flex -mx-3">
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-default font-normal text-lg mb-3">Tasks</h2>
-                    @foreach($project->tasks as $task)
+                    <h2 class="text-lg text-muted font-light mb-3">Tasks</h2>
+
+                    {{-- tasks --}}
+                    @foreach ($project->tasks as $task)
                         <div class="card mb-3">
-                            <form method="POST" action="{{$task->path()}}">
+                            <form method="POST" action="{{ $task->path() }}">
                                 @method('PATCH')
                                 @csrf
 
-                                <div class="flex">
-                                    <input name="body"
-                                           class="bg-card text-default w-full {{$task->completed ? 'text-default' : ''}}"
-                                           value="{{$task->body}}"/>
-                                    <input type="checkbox" name="completed"
-                                           onchange="this.form.submit();"{{$task->completed ? 'checked' : ''}}/>
+                                <div class="flex items-center">
+                                    <input name="body" value="{{ $task->body }}"
+                                           class="text-default bg-card w-full {{ $task->completed ? 'line-through text-muted' : '' }}">
+                                    <input name="completed" type="checkbox"
+                                           onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
                             </form>
                         </div>
                     @endforeach
 
                     <div class="card mb-3">
-                        <form action="{{$project->path() ."/tasks"}}" method="POST">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
                             @csrf
-                            <input name="body" class="bg-card text-default w-full" placeholder="Add a new task"/>
+
+                            <input placeholder="Add a new task..." class="text-default bg-card w-full" name="body">
                         </form>
                     </div>
                 </div>
 
                 <div>
-                    <h2 class="text-default font-normal text-lg mb-3">General Notes</h2>
+                    <h2 class="text-lg text-muted font-light mb-3">General Notes</h2>
 
-                    {{-- general notes--}}
-                    <form method="POST" action="{{$project->path()}}">
+                    {{-- general notes --}}
+                    <form method="POST" action="{{ $project->path() }}">
                         @csrf
                         @method('PATCH')
-                        <textarea name="notes" class="card w-full mb-4" style="min-height: 200px;"
-                                  placeholder="Anything special that you want to make a note of?">{{$project->notes}}</textarea>
+
+                        <textarea
+                            name="notes"
+                            class="card text-default w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
+
                         <button type="submit" class="button">Save</button>
                     </form>
 
-                    @include('errors')
+                    @include ('errors')
                 </div>
             </div>
-            <div class="lg:w-1/4 px-3 lg:py-8">
-                @include('projects.card')
 
-                @include('projects.activity.card')
+            <div class="lg:w-1/4 px-3 lg:py-8">
+                @include ('projects.card')
+                @include ('projects.activity.card')
 
                 @can ('manage', $project)
-                    @include('projects.invite');
+                    @include ('projects.invite')
                 @endcan
             </div>
         </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ require('laravel-mix-tailwind');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .tailwind();
